7c711ac7cd8bec7f0795e282e3a8b53dad1ed41b
silveranon323 <101815577+silveranon323@users.noreply.github.com>
2025-10-21 14:08:02 +0530

    Done with writting all the api routes

---
+++ user-service/src/main/java/com/paypal/user_service/entity/User.java
@@ -0,0 +1,40 @@
+package com.paypal.user_service.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "app_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
