99c9b0bd8674945a8fd661fc098f3947e43ab86d
silveranon323 <101815577+silveranon323@users.noreply.github.com>
2025-10-23 21:01:03 +0530

    Implemented Transaction controller

--- transaction-service/src/main/java/com/paypal/transaction_service/service/TransactionServiceImpl.java
+++ transaction-service/src/main/java/com/paypal/transaction_service/service/TransactionServiceImpl.java
@@ -3,10 +3,11 @@ package com.paypal.transaction_service.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.paypal.transaction_service.entity.Transaction;
 import com.paypal.transaction_service.repository.TransactionRepository;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
+@Service
 public class TransactionServiceImpl implements TransactionService {
     private final TransactionRepository repository;
 
